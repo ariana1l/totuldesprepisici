4c5378425ce104456ef173f305ac39b477819309
ariana1l <168214477+ariana1l@users.noreply.github.com>
2024-04-27 15:02:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href = "Index.html";
+}
+
+function openContact() {
+    window.location.href = "contact.html";
+}
+
+function openReviews() {
+    window.location.href = "reviews.html";
+}
